d40a72b394d63a2de6ea657f77865f8e76eeb2ff
garrettjoecox <garrettjcox@gmail.com>
2015-04-01 19:53:20 -0700

    (feat) Added twitter API testing

---
+++ server/api/twitter.js
@@ -0,0 +1,30 @@
+
+var OAuth2 = require('OAuth').OAuth2;
+var keys = require('../keys.js');
+var https = require('https');
+
+var oauth2 = new OAuth2(keys.twitter.key, keys.twitter.secret, 'https://api.twitter.com/', null, 'oauth2/token', null);
+
+oauth2.getOAuthAccessToken('', {grant_type: 'client_credentials'}, function (e, token) {
+
+  var options = {
+    hostname: 'api.twitter.com',
+    path: '/1.1/trends/place.json?id=1',
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
+  };
+
+  https.get(options, function(result) {
+    var buffer = '';
+    result.setEncoding('utf8');
+    result.on('data', function(data) {
+      buffer += data;
+    });
+
+    result.on('end', function() {
+      var tweets = JSON.parse(buffer);
+      console.log(tweets);
+    });
+  });
+});
