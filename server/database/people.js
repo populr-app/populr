3f3ab2a7fcf96a736ff3e6cf984e1ed82d109b7c
Mark Marcantano <m.marcantano@gmail.com>
2015-04-03 16:11:27 -0700

    (add) twitter counter id to database schema and add postgres heroku url env var

--- server/database/people.js
+++ server/database/people.js
@@ -1,17 +1,18 @@
 
 // Database model for everyone in our database
 
-var uri = process.env.POSTGRES_URI || 'postgres://localhost:5432/yourname';
+var uri = process.env.HEROKU_POSTGRESQL_OLIVE_URL || 'postgres://localhost:5432/yourname';
 var Sequelize = require('sequelize');
 var sql = new Sequelize(uri);
 
 module.exports = sql.define('people', {
-  identifier: { type: Sequelize.STRING, primaryKey: true },
+  id: { type: Sequelize.UUID, primaryKey: true, defaultValue: Sequelize.UUIDV1 allowNull: false },
   firstName: { type: Sequelize.STRING },
   lastName: { type: Sequelize.STRING },
   profession: { type: Sequelize.STRING },
   score: { type: Sequelize.INTEGER, default: 0},
   scoreChange: { type: Sequelize.INTEGER, default: 0},
+  lastUpdate: {type: }
 });
 
 module.exports.sync();
