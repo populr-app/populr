13ee37370cebe328fbcd45a96995d90a127e99ad
garrettjoecox <garrettjcox@gmail.com>
2015-04-04 17:23:08 -0700

    (fix) Change some schema stuff, disable postgres logging

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,8 @@
 var Sequelize = require('sequelize');
 
 var uri = process.env.HEROKU_POSTGRESQL_OLIVE_URL || 'postgres://localhost:5432/yourname';
-var sql = new Sequelize(uri);
+var sql = new Sequelize(uri, {
+  logging: false
+});
 
 module.exports = sql;
