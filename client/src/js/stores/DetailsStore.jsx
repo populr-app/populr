55e73c72d8c7e66a15b6ff8fff59ddb139e66655
Mark Marcantano <m.marcantano@gmail.com>
2015-04-06 11:52:41 -0700

    (add) react stores

---
+++ client/src/js/stores/DetailsStore.jsx
@@ -0,0 +1,24 @@
+var Reflux = require('reflux');
+var DetailsActions = require('../actions/DetailsActions.jsx');
+
+var _details = [];
+
+var PeopleStore = Reflux.createStore({
+  init: function(){
+    this.load();
+    this.listenTo(DetailsActions.loadDetails, this.load);
+  },
+  load: function(){
+    var context = this;
+      $.ajax({
+        type: "GET",
+        url: '/api/people/' + ,
+      }).done(function(data){
+          console.log(data);
+          _details = data; //push data to store
+          context.trigger(_details;
+      });
+  }
+});
+
+module.exports = DetailsStore;  
