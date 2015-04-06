666d2fe56ef4fdd5905bb5b6ab5cb30d4fab1ce1
Mark Marcantano <m.marcantano@gmail.com>
2015-04-06 15:29:37 -0700

    (fix) google analytics component to render correctly

--- client/src/js/components/partials/GoogleAnalytics.js
+++ client/src/js/components/partials/GoogleAnalytics.js
@@ -1,7 +1,5 @@
 var GoogleAnalytics = React.createClass({
   render: function(){
-    return (
-      <script>
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -9,8 +7,8 @@ var GoogleAnalytics = React.createClass({
 
         ga('create', 'UA-61617205-1', 'auto');
         ga('send', 'pageview');
-      </script> 
-  )}
+    return (<div className="google-analytics"></div>
+      )}
 });
 
 module.exports = GoogleAnalytics;
