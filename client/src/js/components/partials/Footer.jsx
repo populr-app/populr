b16fe947e23fcca9e9483133c1cfff8de517b886
Mark Marcantano <m.marcantano@gmail.com>
2015-04-06 15:06:53 -0700

    (add) footer file

---
+++ client/src/js/components/partials/Footer.jsx
@@ -0,0 +1,11 @@
+var Footer = React.createClass({
+  render: function(){
+    return (
+      <div className="footer">
+        Copyright Date.getFullYear(); | Populr 
+      </div>
+      )
+  }
+});
+
+module.exports = Footer;
