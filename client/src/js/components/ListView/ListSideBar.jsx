4b07b3cceed5e413e6fac76256f8bb798df2953c
William Wung <williamcwung@gmail.com>
2015-04-06 15:54:10 -0700

    (add) People navigation markup

--- client/src/js/components/ListView/ListSideBar.jsx
+++ client/src/js/components/ListView/ListSideBar.jsx
@@ -1,11 +1,13 @@
 var Logo = require('../partials/Logo.jsx');
 var Footer = require('../partials/Footer.jsx');
+var PeopleNav = require('../partials/PeopleNav.jsx');
 
 var ListSideBar = React.createClass({
   render: function(){
     return (
         <div className="list-sidebar">
           <Logo />
+          <PeopleNav />
           <Footer />
         </div>
       );
