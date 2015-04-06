3607cadd279722d4d6e8743eedebdcd5624f68a2
Mark Marcantano <m.marcantano@gmail.com>
2015-04-06 15:11:05 -0700

    (add) footer to list and details sidebar)

--- client/src/js/components/DetailsView/DetailsSideBar.jsx
+++ client/src/js/components/DetailsView/DetailsSideBar.jsx
@@ -1,10 +1,12 @@
 var Logo = require('../partials/Logo.jsx');
+var Footer = require('../partials/Footer.jsx');
 
 var DetailsSidebar = React.createClass({
   render: function(){
     return (
         <div className="details-sidebar">
           <Logo />
+          <Footer />
         </div>
       );
   }
