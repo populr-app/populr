0cb6d4d7657950090b21a98cb1aec69f18968b6f
Mark Marcantano <m.marcantano@gmail.com>
2015-04-06 20:12:23 -0700

    (add) demo data hard-coded into peopleStore, displaying lists on front-end

--- client/src/js/components/ListView/ListMain.jsx
+++ client/src/js/components/ListView/ListMain.jsx
@@ -4,13 +4,23 @@ var PeopleStore = require('../../stores/PeopleStore.jsx');
 
 var ListMain = React.createClass({
   getInitialState: function(){
-    return { people: PeopleStore.getPeople() };
+    return { 
+      aList: PeopleStore.getList("A"),
+      bList: PeopleStore.getList("B"),
+      cList: PeopleStore.getList("C"),
+      dList: PeopleStore.getList("D"),
+     };
   },
   render: function(){
     return (
         <div className="list-main">
           <ListHeader />
-          <ListContainer people={this.state.people} />
+          <ListContainer 
+            aList={this.state.aList}
+            bList={this.state.bList}
+            cList={this.state.cList}
+            dList={this.state.dList}
+          />
         </div>
       );
   }
