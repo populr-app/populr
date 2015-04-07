a88aabcc7768bbfd33ee971e9811adb54f30a490
William Wung <williamcwung@gmail.com>
2015-04-07 12:31:26 -0700

    (implement) styling to main container, list item

--- client/src/js/components/ListView/ListHeader.jsx
+++ client/src/js/components/ListView/ListHeader.jsx
@@ -3,10 +3,12 @@ var ListHeader = React.createClass({
     return (
         <div className="list-header header-block">
         	<div className="container">
-        		<h1 className="list-header__title">Intelligent Social Metrics</h1>
-        		<p className="list-header__about">
-        			louis vuitton loubatins fendi guess levis yves saint laurent valentino dior versace armani gucci prada dolce and gabanna. Chanel gucci louboutins christian dior georgio armani. louis vuitton loubatins fendi guess levis yves saint laurent valentino dior versace armani gucci prada dolce and gabanna.
-        		</p>
+        		<div className="row">
+	        		<h1 className="list-header__title col-md-12 col-sm-12">Intelligent Social Metrics</h1>
+	        		<p className="list-header__about col-sm-12">
+	        			louis vuitton loubatins fendi guess levis yves saint laurent valentino dior versace armani gucci prada dolce and gabanna. Chanel gucci louboutins christian dior georgio armani. louis vuitton loubatins fendi guess levis yves saint laurent valentino dior versace armani gucci prada dolce and gabanna.
+	        		</p>
+	        	</div>
         	</div>
         </div>
       );
