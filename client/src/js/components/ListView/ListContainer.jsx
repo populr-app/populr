e53c3482615004ef41a07e65e60d3bc12acaa88c
William Wung <williamcwung@gmail.com>
2015-04-07 09:59:19 -0700

    (update) container class and header classes

--- client/src/js/components/ListView/ListContainer.jsx
+++ client/src/js/components/ListView/ListContainer.jsx
@@ -5,40 +5,42 @@ var ListContainer = React.createClass({
     console.log(this.props.people);
     return (
         <div className="list-container">
-          <div className="a-list-container">
-            <h2>A-List</h2>
-            <ul className="people-list">
-              {this.props.aList.map(function(person) {
-                return <ListItem key={person.rank} person={person} />
-              })}
-            </ul>
-          </div>
+          <div className="container">
+            <div className="a-list-container">
+              <h2 className="list-container__title">A-List</h2>
+              <ul className="people-list">
+                {this.props.aList.map(function(person) {
+                  return <ListItem key={person.rank} person={person} />
+                })}
+              </ul>
+            </div>
 
-          <div className="b-list-container">
-            <h2>B-List</h2>
-            <ul className="people-list">
-              {this.props.bList.map(function(person) {
-                return <ListItem key={person.rank} person={person} />
-              })}
-            </ul>
-          </div>
+            <div className="b-list-container">
+              <h2 className="list-container__title">B-List</h2>
+              <ul className="people-list">
+                {this.props.bList.map(function(person) {
+                  return <ListItem key={person.rank} person={person} />
+                })}
+              </ul>
+            </div>
 
-          <div className="c-list-container">
-            <h2>C-List</h2>
-            <ul className="people-list">
-              {this.props.cList.map(function(person) {
-                return <ListItem key={person.rank} person={person} />
-              })}
-            </ul>
-          </div>
+            <div className="c-list-container">
+              <h2 className="list-container__title">C-List</h2>
+              <ul className="people-list">
+                {this.props.cList.map(function(person) {
+                  return <ListItem key={person.rank} person={person} />
+                })}
+              </ul>
+            </div>
 
-          <div className="d-list-container">
-            <h2>C-List</h2>
-            <ul className="people-list">
-              {this.props.dList.map(function(person) {
-                return <ListItem key={person.rank} person={person} />
-              })}
-            </ul>
+            <div className="d-list-container">
+              <h2 className="list-container__title">C-List</h2>
+              <ul className="people-list">
+                {this.props.dList.map(function(person) {
+                  return <ListItem key={person.rank} person={person} />
+                })}
+              </ul>
+            </div>
           </div>
          </div> 
       );
