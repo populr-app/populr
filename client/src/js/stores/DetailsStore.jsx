1b502cf66fdeec8a4818961cf3b1b74c57dbabe8
William Wung <williamcwung@gmail.com>
2015-04-06 22:32:37 -0700

    (fix) code style

--- client/src/js/stores/DetailsStore.jsx
+++ client/src/js/stores/DetailsStore.jsx
@@ -4,21 +4,24 @@ var DetailsActions = require('../actions/DetailsActions.jsx');
 var _details = [];
 
 var DetailsStore = Reflux.createStore({
-  init: function(){
+  init: function() {
     this.listenTo(DetailsActions.loadDetails, this.load);
   },
-  load: function(name){
-    var context = this;
-      $.ajax({
-        type: "GET",
-        url: '/api/people/' + name,
-      }).done(function(data){
-          console.log(data);
-          _details = data; //push data to store
-          context.trigger(_details);
+
+  load: function(name) {
+    var _this = this;
+    $.ajax({
+        type: 'GET',
+        url: '/api/people/' + name
+      }).done(function(data) {
+        console.log(data);
+        _details = data;
+        _this.trigger(_details);
       });
-      return _details;
+
+    return _details;
   }
 });
 
-module.exports = DetailsStore;  
+module.exports = DetailsStore;
+
