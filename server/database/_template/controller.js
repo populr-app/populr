aa89ad8da3507f5d11cc5f931d6b96c93ebc79f8
garrettjoecox <garrettjcox@gmail.com>
2015-04-08 10:43:26 -0700

    (cleanup) Major cleanup & add finalized logging for db stuff

--- server/database/_template/controller.js
+++ server/database/_template/controller.js
@@ -1,4 +1,13 @@
 
+/*
+ * Template
+ * template
+ * Highlight the words above, hold cmd + shift + D
+ * replace them with the name of your db
+ * Make sure to capitalize the first letter while on the first change
+ * and delete this block when done!
+ */
+
 var Template = require('./model');
 var log = require('../../helpers/logger').log;
 
@@ -18,10 +27,13 @@ module.exports.query = function(query) {
   if (!query) {
     return null;
   } else {
+    log('${a}: Checking template table', query.where.id || query.where.fullName);
     return Template.findOne(query).then(function(foundTemplate) {
       if (!foundTemplate) {
+        log('${a}: Not found in template table', query.where.id || query.where.fullName);
         return null;
       } else {
+        log('${a}: Found in template table', query.where.id || query.where.fullName);
         return foundTemplate.get();
       }
     });
@@ -39,6 +51,7 @@ module.exports.attachData = function(personObj) {
       if (!foundTemplate) {
         return personObj;
       } else {
+        log('${a}: Attaching template data', personObj.id);
         personObj.template = foundTemplate;
         return personObj;
       }
@@ -48,16 +61,22 @@ module.exports.attachData = function(personObj) {
 
 // Adds or updates an entry in the database, takes a person object
 // that has an ID and a given dataset
-// TODO: Update
+// Ex: {id: '', template: {}}
 module.exports.add = function(personObj) {
   if (!personObj.template) {
     return personObj;
   } else {
     var query = { where: { id: personObj.id } };
-    return module.exports.query(query).then(function(foundTemplate) {
+    log('${a}: Checking template table', query.where.id || query.where.fullName);
+    return Template.findOne(query).then(function(foundTemplate) {
       if (foundTemplate) {
+        log('${a}: Found in template table', query.where.id || query.where.fullName);
+        log('${a}: Updating template data', query.where.id || query.where.fullName);
+        foundTemplate.update(personObj.template);
         return personObj;
       } else {
+        log('${a}: Not found in template table', query.where.id || query.where.fullName);
+        log('${a}: Creating entry in template table', query.where.id || query.where.fullName);
         personObj.template.id = personObj.id;
         return Template.create(personObj.template).then(function(newTemplate) {
           return personObj;
--- server/database/twitter/controller.js
+++ server/database/twitter/controller.js
@@ -18,10 +18,13 @@ module.exports.query = function(query) {
   if (!query) {
     return null;
   } else {
+    log('${a}: Checking twitter table', query.where.id || query.where.fullName);
     return Twitter.findOne(query).then(function(foundTwitter) {
       if (!foundTwitter) {
+        log('${a}: Not found in twitter table', query.where.id || query.where.fullName);
         return null;
       } else {
+        log('${a}: Found in twitter table', query.where.id || query.where.fullName);
         return foundTwitter.get();
       }
     });
@@ -39,6 +42,7 @@ module.exports.attachData = function(personObj) {
       if (!foundTwitter) {
         return personObj;
       } else {
+        log('${a}: Attaching twitter data', personObj.id);
         personObj.twitter = foundTwitter;
         return personObj;
       }
@@ -48,18 +52,22 @@ module.exports.attachData = function(personObj) {
 
 // Adds or updates an entry in the database, takes a person object
 // that has an ID and a given dataset
-// TODO: Update
 // Ex: {id: '', twitter: {}}
 module.exports.add = function(personObj) {
   if (!personObj.twitter) {
     return personObj;
   } else {
     var query = { where: { id: personObj.id } };
+    log('${a}: Checking twitter table', query.where.id || query.where.fullName);
     return Twitter.findOne(query).then(function(foundTwitter) {
       if (foundTwitter) {
+        log('${a}: Found in twitter table', query.where.id || query.where.fullName);
+        log('${a}: Updating twitter data', query.where.id || query.where.fullName);
         foundTwitter.update(personObj.twitter);
         return personObj;
       } else {
+        log('${a}: Not found in twitter table', query.where.id || query.where.fullName);
+        log('${a}: Creating entry in twitter table', query.where.id || query.where.fullName);
         personObj.twitter.id = personObj.id;
         return Twitter.create(personObj.twitter).then(function(newTwitter) {
           return personObj;
