8c1f9dc9f6e1501e0189b5d7794e12ec3d0a71d0
garrettjoecox <garrettjcox@gmail.com>
2015-04-09 12:34:19 -0700

    (add) TESTING! :D

---
+++ server/serverSpec.js
@@ -0,0 +1,12 @@
+
+var app = require('./config.js');
+var expect = require('chai').expect;
+var request = require('supertest');
+
+describe('', function() {
+  describe('SPECCSS', function(){
+    it('should work...', function(){
+      expect(true).to.be.true;
+    });
+  });
+});
