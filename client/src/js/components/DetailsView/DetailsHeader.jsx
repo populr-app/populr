3948550f20c3f2027e43f05e87f599cfdf8c0cee
William Wung <williamcwung@gmail.com>
2015-04-11 17:20:31 -0700

    (implement) display of ranking with ordinal suffix

--- client/src/js/components/DetailsView/DetailsHeader.jsx
+++ client/src/js/components/DetailsView/DetailsHeader.jsx
@@ -5,17 +5,41 @@ var DetailsHeader = React.createClass({
         backgroundPic: '',
         profilePic: ''
       };
+      this.props.details.context = {
+        occupation: ''
+      };
     }
-    var dynamicBackground = { 
-      backgroundImage: 'url(' + this.props.details.twitter.backgroundPic + ')',
-      backgroundSize: '100%',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center center'
-    };
+
+    if (this.props.details.top === undefined) {
+      this.props.details.top = {
+        rank: 0
+      };
+    }
+
+    /* Adds an ordinal suffix to the rank number */
+    var rank = this.props.details.top.rank;
+    var rankSuffix = '';
+    function ordinal_suffix_of(i) {
+      var j = i % 10,
+          k = i % 100;
+      if (j == 1 && k != 11) {
+          return i + "st";
+      }
+      if (j == 2 && k != 12) {
+          return i + "nd";
+      }
+      if (j == 3 && k != 13) {
+          return i + "rd";
+      }
+      return i + "th";
+    }
+
+    rankSuffix = ordinal_suffix_of(rank);
 
     /* Removes '_normal' in uri to retrieve larger Twitter picture */
     var profilePicture = this.props.details.twitter.profilePic;
     profilePicture = profilePicture.replace(/_normal/i, '');
+
     return (
         <div className="details-header">
         	<div className="container">
@@ -33,7 +57,7 @@ var DetailsHeader = React.createClass({
               </div>
             </div>
             <div className="details-header__rank inline">
-              <span>Rank 4<sup>th</sup></span>
+              <span>Ranked {rankSuffix}</span>
             </div>
           </div>
         </div>
