5393a5d818a92e7a1c92e2eddf21dde2456eece3
Mark Marcantano <m.marcantano@gmail.com>
2015-04-12 16:35:07 -0700

    (fix) changes db variable to new, paid postgres instance

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 
 var Sequelize = require('sequelize');
 
-var uri = process.env.HEROKU_POSTGRESQL_OLIVE_URL || 'postgres://localhost:5432/yourname';
+var uri = process.env.HEROKU_POSTGRESQL_AMBER_URL || 'postgres://localhost:5432/yourname';
 var sql = new Sequelize(uri, {
   logging: false
 });
