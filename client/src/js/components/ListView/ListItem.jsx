552965194c9a5fac7afc140839e13eb43181ae57
Mark Marcantano <m.marcantano@gmail.com>
2015-04-12 17:37:44 -0700

    (fix) list item view of occupation and change in score

--- client/src/js/components/ListView/ListItem.jsx
+++ client/src/js/components/ListView/ListItem.jsx
@@ -2,6 +2,7 @@ var Link = require('react-router').Link;
 
 var ListItem = React.createClass({
   render: function(){
+    console.log(this.props.person)
     return (
       <li className="person-item">
         <div className="person-rank-circle">
@@ -11,9 +12,9 @@ var ListItem = React.createClass({
         <div className="person-details">
           <Link to="details" params={this.props.person}>
             <span className="person-name">{this.props.person.fullName}</span>
-            <span className="person-profession">{this.props.person.occupation || 'Celebrity'}</span>
+            <span className="person-profession">{this.props.person.occupation}</span>
             <span className="person-score">{this.props.person.score}</span>
-            <span className="person-score-change">{this.props.person.scoreChange}</span>
+            <span className="person-score-change">{this.props.person.scorechange}</span>
           </Link>
         </div>
         </div>
