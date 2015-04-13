07043498769fe512ac08da58999769e5dbed897f
William Wung <williamcwung@gmail.com>
2015-04-13 16:08:29 -0700

    (add) ViewNav component to Details Sidebar

--- client/src/js/components/DetailsView/DetailsSideBar.jsx
+++ client/src/js/components/DetailsView/DetailsSideBar.jsx
@@ -1,11 +1,13 @@
 var Logo = require('../partials/Logo.jsx');
 var Footer = require('../partials/Footer.jsx');
+var ViewNav = require('../partials/ViewNav.jsx');
 
 var DetailsSidebar = React.createClass({
   render: function(){
     return (
         <div className="details-sidebar">
           <Logo />
+          <ViewNav />
           <Footer />
         </div>
       );
