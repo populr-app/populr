dc0048b4b74d4df52f60913eb185ba7ca77304d8
Mark Marcantano <m.marcantano@gmail.com>
2015-04-14 16:55:46 -0700

    (fix) removed dynamic tweet generation to fix details page

--- client/src/js/components/DetailsView/DetailsModules/TwitterFeed.jsx
+++ client/src/js/components/DetailsView/DetailsModules/TwitterFeed.jsx
@@ -2,12 +2,12 @@ var Tweet = require('./Tweet.jsx');
 
 var TwitterFeed = React.createClass({
   render: function(){
+          // {this.props.tweets.map(function(tweet){
+          //   tweet = JSON.stringify(tweet);
+          //   return <Tweet key={tweet.timestamp} tweet={person} />
+          // })}
     return (
         <div className="twitter-feed">
-          {this.props.tweets.map(function(tweet){
-            tweet = JSON.stringify(tweet);
-            return <Tweet key={tweet.timestamp} tweet={person} />
-          })}
         </div>
       );
   }
