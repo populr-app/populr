6be6e633f8ecbdf336235c2d6d72ed19d42b96f0
garrettjoecox <garrettjcox@gmail.com>
2015-04-13 17:20:40 -0700

    (Fix) Attempt 2 at codeship PG

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,9 @@
 var Sequelize = require('sequelize');
 
 var uri = 'postgres://localhost:5432/yourname';
-if (process.env.PG_USER) url = 'postgres://' + process.env.PG_USER + ':' + process.env.PG_PASSWORD + '@localhost/test';
+console.log(process.env.PG_USER);
+console.log(process.env.PG_PASSWORD);
+if (process.env.PG_USER) url = 'postgres://' + process.env.PG_USER + ':' + process.env.PG_PASSWORD + '@localhost:5432/test';
 if (process.env.HEROKU_POSTGRESQL_AMBER_URL) url = process.env.HEROKU_POSTGRESQL_AMBER_URL;
 
 var sql = new Sequelize(uri, {
