137e5a476ea9cfd1e93a1ea5dfe121a106f1b477
garrettjoecox <garrettjcox@gmail.com>
2015-04-13 18:44:24 -0700

    (update) Now sending DOB & Occupation back to client on Top endpoint

--- server/workers/topUpdater.js
+++ server/workers/topUpdater.js
@@ -1,19 +1,23 @@
 
 var People = require('../database/people/model');
 var Top = require('../database/top/model');
+var Context = require('../database/context/controller');
 
 module.exports = function() {
   return Top.drop().then(function() {
     return Top.sync();
   }).then(function() {
-    People.findAll({ limit: 200, order: 'score DESC' }).then(function(data) {
-      if (data.length) {
-        for (var i = 0; i < data.length; i++) {
-          var person = data[i].get();
-          person.rank = i + 1;
-          Top.create(person);
-        }
-      }
-    });
+    return People.findAll({ limit: 200, order: 'score DESC' });
+  }).each(function(person, i) {
+    if (person) {
+      person = person.get();
+      person.rank = i + 1;
+      return Context.attachData(person)
+        .then(function(newPerson) {
+          newPerson.dob = newPerson.context.dob;
+          newPerson.occupation = newPerson.context.occupation;
+          return Top.create(newPerson);
+        });
+    }
   });
 };
