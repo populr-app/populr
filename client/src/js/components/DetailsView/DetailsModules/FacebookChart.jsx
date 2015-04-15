d7eafb7627bd8963edadc5d256ae0aa042fae2aa
Mark Marcantano <m.marcantano@gmail.com>
2015-04-15 10:26:53 -0700

    (add) charts for every social details section

--- client/src/js/components/DetailsView/DetailsModules/FacebookChart.jsx
+++ client/src/js/components/DetailsView/DetailsModules/FacebookChart.jsx
@@ -3,12 +3,13 @@ var LineChart = Chart.Line;
 
 
 
-var TwitterChart = React.createClass({
+var FacebookChart = React.createClass({
   render: function(){
     var demoData = {
       labels: [24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1],
       datasets: [
           {
+            scaleLabel: "Hours Ago",
             label: "Twitter Score",
             fillColor: "rgba(0,188,212,0.2)",
             strokeColor: "rgba(0,188,212,1)",
@@ -31,4 +32,4 @@ var TwitterChart = React.createClass({
   }
 });
 
-module.exports = TwitterChart;
+module.exports = FacebookChart;
