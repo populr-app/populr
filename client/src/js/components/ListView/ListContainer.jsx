a129444f078e41875968d8328c54e3bc4747bcb9
William Wung <williamcwung@gmail.com>
2015-04-14 17:15:27 -0700

    (update) sections with wrappers

--- client/src/js/components/ListView/ListContainer.jsx
+++ client/src/js/components/ListView/ListContainer.jsx
@@ -7,9 +7,11 @@ var ListContainer = React.createClass({
     return (
         <div className="list-container">
           <div className="container">
-            <span className="last-updated">Updated: 15 Minutes Ago</span>
+            <span className="last-updated">Updated: 15 minutes ago</span>
             <div id="a-list" className="list-container__section">
-              <h2 className="list-container__title">A-List</h2>
+              <div className="list-container--title-wrapper">
+                <h2 className="list-container__title">A-List</h2>
+              </div>
               <ul className="people-list">
               {this.props.aList.map(function(person){
                 return <ListItem key={person.fullName} person={person} />
@@ -18,7 +20,9 @@ var ListContainer = React.createClass({
             </div>
 
             <div id="b-list" className="list-container__section">
-              <h2 className="list-container__title">B-List</h2>
+              <div className="list-container--title-wrapper">
+                <h2 className="list-container__title">B-List</h2>
+              </div>
               <ul className="people-list">
                 {this.props.bList.map(function(person){
                   return <ListItem key={person.fullName} person={person} />
@@ -27,7 +31,9 @@ var ListContainer = React.createClass({
             </div>
 
             <div id="c-list" className="list-container__section">
-              <h2 className="list-container__title">C-List</h2>
+              <div className="list-container--title-wrapper">
+                <h2 className="list-container__title">C-List</h2>
+              </div>
               <ul className="people-list">
                 {this.props.cList.map(function(person){
                   return <ListItem key={person.fullName} person={person} />
@@ -36,7 +42,9 @@ var ListContainer = React.createClass({
             </div>
 
             <div id="d-list" className="list-container__section">
-              <h2 className="list-container__title">D-List</h2>
+              <div className="list-container--title-wrapper">
+                <h2 className="list-container__title">D-List</h2>
+              </div>
               <ul className="people-list">
                 {this.props.dList.map(function(person){
                   return <ListItem key={person.fullName} person={person} />
