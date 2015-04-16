523d0d0badea03e642939866c836f0d77577d9b2
garrettjoecox <garrettjcox@gmail.com>
2015-04-16 09:44:57 -0700

    (fix) Periodic score updater (twitter)

--- server/workers/twitterScraper.js
+++ server/workers/twitterScraper.js
@@ -103,23 +103,23 @@ function calculateScores(max) {
 
       person.scorecounter++;
 
-      if (((person.scorecounter / 6) / 24) % 7 === 0) {
+      if (person.counter / 6 / 24 % 7 === 0 && person.counter !== 0) {
         person.scoreweek.unshift(average(person.scoreday));
-        if (person.scoreweek.length > 6) person.scoreweek.pop();
+        if (person.scoreweek.length > 4) person.scoreweek.pop();
       }
 
-      if ((person.scorecounter / 6) % 24 === 0) {
+      if (person.counter / 6 % 24 === 0 && person.counter !== 0) {
         person.scoreday.unshift(average(person.scorehour));
-        if (person.scoreday.length > 23) person.scoreday.pop();
+        if (person.scoreday.length > 7) person.scoreday.pop();
       }
 
-      if (person.scorecounter % 6 === 0) {
+      if (person.counter % 6 === 0 && person.counter !== 0) {
         person.scorehour.unshift(average(person.scoreminute));
-        if (person.scorehour.length > 5) person.scorehour.pop();
+        if (person.scorehour.length > 24) person.scorehour.pop();
       }
 
       person.scoreminute.unshift(score);
-      if (person.scoreminute.length > 5) person.scoreminute.pop();
+      if (person.scoreminute.length > 6) person.scoreminute.pop();
 
       var update = {
         fullName: person.fullName,
