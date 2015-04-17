76da49c540f616024876d7e3213c0722e2218e8f
dannydelott <dannydelott@gmail.com>
2015-04-17 15:48:41 -0700

    (init) scaffold facebook worker

--- server/workers/facebook.js
+++ server/workers/facebook.js
@@ -1,15 +1,35 @@
-var graph = require('fbgraph');
+
+var FacebookApi = require('fbgraph');
+var Promise = require('bluebird');
+var FacebookDB = require('../database/facebook/model.js');
+var FacebookController = require('../database/facebook/controller.js');
 var keys = require('../../keys.js').facebook;
+
+Promise.promisifyAll(FacebookApi.prototype);
+
+// set auth credentials for Facebook Graph API
+FacebookApi
+  .setAccessToken(keys.accessToken)
+  .setAppSecret(keys.appSecret);
+
+module.exports = function(){
+  return FacebookDB.findAll()
+  .then(updatePages);
+}
+
+// Query facebook table for people
+// iterate over people
+//  for each person, loop over pages
+//    for each page, update to include the newest information from fb api
+//    save person in facebook table
+
 
 var searchOptions = {
   q:'Katy Perry',
   type: 'page'
 };
 
-graph
-  .setAccessToken(keys.accessToken)
-  .setAppSecret(keys.appSecret)
-   .get("1392443837682836", function(err, res) {
+FacebookApi.get("1392443837682836", function(err, res) {
     console.log(res); // { id: '4', name: 'Mark Zuckerberg'... }
   });
   // .search(searchOptions, function(err, res) {
