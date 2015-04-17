794719e02aa35c41cd7353dd88bc786e6de5ad9d
dannydelott <dannydelott@gmail.com>
2015-04-16 17:24:12 -0700

    (feat) filter persons facebook pages
    - Keep the top 3 most-liked facebook pages on each person.

--- data/dataCleaner.js
+++ data/dataCleaner.js
@@ -1,13 +1,12 @@
 var fs = require('fs');
+var _ = require('lodash');
 
 // read in the clientData.js file
-var peopleObject = JSON.parse(fs.readFileSync('clientData.json', {encoding:'utf8'}));
-
-filterDisambiguationPages(peopleObject);
-cleanUpDescriptions(peopleObject);
+var peopleObject = JSON.parse(fs.readFileSync('clientData3.json', {encoding:'utf8'}));
+filterFacebookPages(peopleObject, 3);
 
 // export new client data
-fs.writeFileSync('clientData2.json', JSON.stringify(peopleObject), {encoding: 'utf8'});
+fs.writeFileSync('clientData4.json', JSON.stringify(peopleObject), {encoding: 'utf8'});
 
 // Filter out people with disambiguation pages or weird links for descriptions.
 function filterDisambiguationPages(peopleObject) {
@@ -29,4 +28,37 @@ function cleanUpDescriptions(peopleObject) {
   });
 }
 
+// Keep the n most-liked facebook pages on each person.
+function filterFacebookPages(peopleObject, n) {
+
+  // Loop over each person
+  peopleObject.people = peopleObject.people.map(function(person) {
+
+    if (!person.facebook) { return person; }
+
+    // pages  => pluck the array of page likes,
+    // sorted => sort it from largest to smallest (desc),
+    // nPages => keep the n most-liked pages
+    var pages = _.pluck(person.facebook.pages, 'likes');
+    var sorted = pages.sort(function(a, b) {return b - a;});
+    var nPages = _.take(sorted, n);
+
+    // filter out pages that don't match the nPages values
+    var counter = 0;
+    person.facebook.pages = person.facebook.pages.filter(function(page) {
+      var keepPage = false;
+      nPages.forEach(function(numLikes, i) {
+          if (page.likes === numLikes && counter < n) {
+            keepPage = true;
+            counter++;
+          }
+        });
+      return keepPage;
+    });
+
+    return person;
+  });
+
+
+}
 
