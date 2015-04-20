f8e717700cd265f1b905eea5bb4a546fb44da2b3
dannydelott <dannydelott@gmail.com>
2015-04-19 20:47:10 -0700

    (update) split facebook data into chunks

--- server/workers/facebook.js
+++ server/workers/facebook.js
@@ -1,11 +1,11 @@
 
 var FacebookApi = require('fbgraph');
 var Promise = require('bluebird');
+var _ = require('lodash');
 var FacebookDB = require('../database/facebook/model.js');
 var FacebookController = require('../database/facebook/controller.js');
 var keys = require('../../keys.js').facebook;
 var Sleep = require('sleep');
-
 Promise.promisifyAll(FacebookApi);
 
 // set auth credentials for Facebook Graph API
@@ -15,21 +15,35 @@ FacebookApi
 
 //module.exports = function(){
 FacebookDB.findAll()
+ .then(splitIntoChunks)
  .then(getFacebookData);
 
 //}
 
-function getFacebookData(people) {
-  people.forEach(function(person) {
+function splitIntoChunks(people) {
+  console.log('Splitting into chunks');
+  for (var i = people.length - 1; i >= 0; i--) {
+    people[i] = people[i].get();
+  }
 
-    var promises = [];
-    person.pages.forEach(function(page) {
+  return _.chunk(people, 100);
+}
 
-      var t = JSON.parse(page);
-      promises.push(FacebookApi.getAsync(t.id).then(updatePage(page)));
+function getFacebookData(chunks) {
+  var promises = [];
+  chunks.forEach(function(chunk) {
 
-      // avoid rate-limiting by limiting requests to 1/sec
-      Sleep.sleep(1);
+    chunk.forEach(function(person) {
+
+      person.pages.forEach(function(page) {
+
+        var t = JSON.parse(page);
+        promises.push(FacebookApi.getAsync(t.id).then(updatePage(page)));
+
+        // avoid rate-limiting by limiting requests to 1/sec
+        Sleep.sleep(1);
+
+      });
 
     });
   });
@@ -39,16 +53,17 @@ function getFacebookData(people) {
 
 function updatePage(page) {
   return function(facebookData) {
-    var update = {
-      url: facebookData.link,
-      pageName: facebookData.name,
-      likes: facebookData.likes,
-      talkingAbout: facebookData.talking_about_count,
-      wereHere: facebookData.were_here_count,
-      id: facebookData.id
-    };
-    console.log('update', update.pageName);
-    FacebookController.add(update);
+    if (facebookData.name) {
+      var update = {
+        url: facebookData.link,
+        pageName: facebookData.name,
+        likes: facebookData.likes,
+        talkingAbout: facebookData.talking_about_count,
+        wereHere: facebookData.were_here_count,
+        id: facebookData.id
+      };
+      console.log('update', update.pageName);
+    }
 
   };
 
