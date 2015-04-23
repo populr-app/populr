a6dc3f1d32000f07053225a601bc5348b8b7bb70
William Wung <williamcwung@gmail.com>
2015-04-22 21:55:03 -0700

    (add) X time ago as chart labels

--- client/src/js/components/partials/TabsContainer.jsx
+++ client/src/js/components/partials/TabsContainer.jsx
@@ -10,37 +10,42 @@ var StatsBreakdown = require('./StatsBreakdown.jsx');
 
 var TabsContainer = React.createClass({
   render: function() {
-    var followers = this.props.details.twitter.followers;
-    var followersChange = this.props.details.twitter.followerschange;
-
-    /* Percentage output func */
-    function percentageCalc(num) {
-      if (num > 0) {
-        num = num + '%';
-      } else {
-        num = Math.abs(num) + '%';
-      }
-
-      return num;
-    };
-    /* Separates numbers with commas */
-    followersCount = followers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    /* Calculates follower change percentage */
-    followersChange = (followersChange / followers) * 100;
-    followersChange = percentageCalc(followersChange.toFixed(4));
-    
-    var percentStyle = {
-      color: '#4b5086'
-    }
+    /* Creates an array for chart labels X hours ago */
+    var timeAgo = [
+      moment().subtract('hour', 1).format('ha'),
+      moment().subtract('hour', 2).format('ha'),
+      moment().subtract('hour', 3).format('ha'),
+      moment().subtract('hour', 4).format('ha'),
+      moment().subtract('hour', 5).format('ha'),
+      moment().subtract('hour', 6).format('ha'),
+      moment().subtract('hour', 7).format('ha'),
+      moment().subtract('hour', 8).format('ha'),
+      moment().subtract('hour', 9).format('ha'),
+      moment().subtract('hour', 10).format('ha'),
+      moment().subtract('hour', 11).format('ha'),
+      moment().subtract('hour', 12).format('ha'),
+      moment().subtract('hour', 13).format('ha'),
+      moment().subtract('hour', 14).format('ha'),
+      moment().subtract('hour', 15).format('ha'),
+      moment().subtract('hour', 16).format('ha'),
+      moment().subtract('hour', 17).format('ha'),
+      moment().subtract('hour', 18).format('ha'),
+      moment().subtract('hour', 19).format('ha'),
+      moment().subtract('hour', 20).format('ha'),
+      moment().subtract('hour', 21).format('ha'),
+      moment().subtract('hour', 22).format('ha'),
+      moment().subtract('hour', 23).format('ha'),
+      moment().subtract('hour', 24).format('ha')
+    ];
     return (
       <Tabs className="container" tabActive={2}>
         <Tabs.Panel title='Facebook'>
           <h2 className="chart-title">Hourly Facebook Score Trends</h2>
-          <FacebookChart facebook={this.props.details.facebook} />
+          <FacebookChart facebook={this.props.details.facebook} timeAgo={timeAgo} />
         </Tabs.Panel>
         <Tabs.Panel title='Twitter'>
         <h2 className="chart-title">Hourly Twitter Score Trends</h2>
-          <TwitterChart twitter={this.props.details.twitter} />
+          <TwitterChart twitter={this.props.details.twitter} timeAgo={timeAgo}/>
           <div className="row twitter-info">
             <div className="col-md-6">
               <TwitterFeed twitter={this.props.details.twitter} />
@@ -54,7 +59,7 @@ var TabsContainer = React.createClass({
         </Tabs.Panel>
         <Tabs.Panel title='News'>
           <h2 className="chart-title">Appearance in News Headlines</h2>
-          <NewsChart sites={this.props.details.sites} />
+          <NewsChart sites={this.props.details.sites} timeAgo={timeAgo}/>
           <NewsFeed headlines={[1,2,3,4,5]} />
         </Tabs.Panel>
       </Tabs>
