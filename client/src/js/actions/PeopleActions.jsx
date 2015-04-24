decdb8b51d467bd15b165a3049bb24b96ea282a3
William Wung <williamcwung@gmail.com>
2015-04-24 15:45:53 -0700

    (update) indentation to spaces

--- client/src/js/actions/PeopleActions.jsx
+++ client/src/js/actions/PeopleActions.jsx
@@ -2,17 +2,17 @@ var Reflux = require('reflux');
 
 var PeopleActions = Reflux.createActions([
   'loadPeople',
-  'loadComplete',
-  ]);
+  'loadComplete'
+]);
 
 PeopleActions.loadPeople.preEmit = function() {
   $.ajax({
-        type: 'GET',
-        url: 'http://api.populr.io/top'
-      }).done(function(data) {
-        data = JSON.parse(data);
-        PeopleActions.loadComplete(data);
-      });
+    type: 'GET',
+    url: 'http://api.populr.io/top'
+  }).done(function(data) {
+    data = JSON.parse(data);
+    PeopleActions.loadComplete(data);
+  });
 }
 
 module.exports = PeopleActions;
