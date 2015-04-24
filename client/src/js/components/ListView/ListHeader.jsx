471985d4c22561fa8af29489c84b00e6898d2ae1
William Wung <williamcwung@gmail.com>
2015-04-24 15:50:43 -0700

    (update) header and title information

--- client/src/js/components/ListView/ListHeader.jsx
+++ client/src/js/components/ListView/ListHeader.jsx
@@ -7,7 +7,7 @@ var ListHeader = React.createClass({
 	        		<h1 className="list-header__title col-md-12 col-sm-12">Intelligent Social Metrics</h1>
 	        		<p className="list-header__about col-sm-12">
 	        			Populr.io takes data from top social networks and news sites to rank the movers and shakers of the internet.
-                Updated every 10 minutes, Populr.io provides the latest on your favorite celebrities, athletes, and public figures.
+                Updated periodically, Populr.io provides the latest on your favorite celebrities, athletes, and public figures.
 	        		</p>
 	        	</div>
         	</div>
