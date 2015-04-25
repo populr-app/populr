4c1474f21b2ce728a425f6b58b7f66c8156b3710
garrettjoecox <garrettjcox@gmail.com>
2015-04-24 19:53:20 -0700

    (update) Only storing 5 recent headlines for each perosn

--- server/workers/sitesScraper.js
+++ server/workers/sitesScraper.js
@@ -137,8 +137,9 @@ function findHeadlines(person, headlines) {
   person.sites.headlines = person.sites.headlines || [];
   headlines.forEach(function(headline) {
     var stringedHeadline = JSON.stringify(headline);
-    if (headline && headline.title && headline.title.indexOf(person.fullName) !== -1 && person.sites.headlines.indexOf(stringedHeadline) === -1) {
+    if (headline.title && headline.title.indexOf(person.fullName) !== -1) {
       person.sites.headlines.push(stringedHeadline);
+      if (person.sites.headlines.length > 5) person.sites.headlines.shift();
     }
   });
 
