0fc887b787c4d7f312c4cb86382e441b589d3321
Mark Marcantano <m.marcantano@gmail.com>
2016-02-12 08:42:28 -0800

    locks node to 0.12

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,7 +1,7 @@
 
 var Sequelize = require('sequelize');
 
-var uri = 'postgres://localhost:5432/yourname';
+var uri = 'postgres://markmarcantano@localhost:5432/markmarcantano';
 if (process.env.PG_USER) uri = 'postgres://' + process.env.PG_USER + ':' + process.env.PG_PASSWORD + '@localhost:5432/test';
 if (process.env.HEROKU_POSTGRESQL_AMBER_URL) uri = process.env.HEROKU_POSTGRESQL_AMBER_URL;
 
